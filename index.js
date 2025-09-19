ef93d9d2c690c70600749c87381816985fe2e578
Aarti-Mern-Stack <aaarti.verma@designersx.com>
2025-09-19 19:15:21 +0530

    [AV] final code here (AV1.1)

--- index.js
+++ index.js
@@ -614,8 +614,8 @@ function injectCSS() {
   document.head.appendChild(style);
 }
 const currentSiteURL = window.location.origin;
-// const API_URL = "https://rexptin.truet.net/api/";
-const API_URL = "http://localhost:2512/api";
+const API_URL = "https://rexpt.truet.net/api/";
+// const API_URL = "http://localhost:2512/api";
 
 // --- CHAT PERSISTENCE ----
 const CHAT_LS_KEY = "rex_chat_history";
